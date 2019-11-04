354d26a0c8fbab928357d5a5ee428a29915651ae
bahayg <bahayg@gmail.com>
2019-11-03 16:55:31 -0800

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,10 @@
 class Song
     attr_accessor :name, :artist, :genre
+    
     @@count = 0
     @@genres = []
     @@artists = []
+
     def initialize(name, artist, genre)
         @name = name
         @artist = artist
